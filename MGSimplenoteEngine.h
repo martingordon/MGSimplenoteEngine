a1d81a90fa7afffc9d88aacd3e5b93185eb2f7bc
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2011-09-14 18:03:27 +0200

    Added the universal header file for the commit back in May

---
+++ MGSimplenoteEngine.h
@@ -0,0 +1,17 @@
+/*
+ *  MGSimplenoteEngine.h
+ *  MGSimplenoteEngine
+ *
+ *  Created by Matthias Hochgatterer on 28.05.11.
+ *  Copyright 2011 selfcoded. All rights reserved.
+ *
+ */
+
+#import "NSData+Base64.h"
+#import "MGSimplenote.h"
+#import "MGSimplenoteCredentialStore.h"
+#import "MGSimplenoteController.h"
+#import "MGSimplenoteSyncSession.h"
+#import "MGSimplenoteSyncStatus.h"
+#import "MGSimplenoteLogin.h"
+
